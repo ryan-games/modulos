b6adb5f424b958490fcf11872b7ffa3c10005220
Ryan Games <ryannavas.m@gmail.com>
2023-08-29 11:57:34 -0300

    Ryan

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,9 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplicacao = require("./modulos/mult");
+var divisao = require("./modulos/media");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multiplicacao(20,90));
+console.log(divisao(20,90));
---
+++ modulos/mult.js
@@ -0,0 +1,4 @@
+var multiplicacao = function (x,y) {
+    return (x * y) ;
+  }; 
+  module.exports = multiplicacao; 
